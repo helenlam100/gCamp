29cbea697381da1fbcc402cbea5553f02d0e6346
Helen Lam <helen.f.lam100@gmail.com>
2014-11-21 15:51:49 -0800

    I have created an array within an array for the quotes at the bottom of the page

--- Gemfile
+++ Gemfile
@@ -32,6 +32,11 @@ group :production do
   gem "rails_12factor"
 end
 
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,10 @@
 class WelcomeController < ApplicationController
   def home
-    @quotes = ["Failure is not an option. Everyone has to succeed.",
-      "- Arnold Schwarzeneggar",
-      "Your time is limited, so don't waste it living someone else's life.",
-      "-Steve Jobs",
-      "Better Ingredients, Better Pizza",
-      "-Pap John's",]
+    @quotes = [["Failure is not an option. Everyone has to succeed.", "-Arnold Schwarzeneggar"],
+    ["Your time is limited, so don't waste it living someone else's life.", "-Steve Jobs"],
+    ["Better Ingredients, Better Pizza", "-Pap John's"],
+    ]
   end
-
-
 end
 
   # def toppings
