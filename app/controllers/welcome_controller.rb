557c5310ecc8ff8e2fb580170e3798759d2cf9eb
Helen Lam <helen.f.lam100@gmail.com>
2014-11-20 16:02:01 -0800

    third commit where I Have added one array to put out a few quotes on the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,20 @@
 class WelcomeController < ApplicationController
   def home
-
+    @quotes = ["Failure is not an option. Everyone has to succeed.",
+      "- Arnold Schwarzeneggar",
+      "Your time is limited, so don't waste it living someone else's life.",
+      "-Steve Jobs",
+      "Better Ingredients, Better Pizza",
+      "-Pap John's",]
   end
+
+
 end
+
+  # def toppings
+  #   @toppings = ['sprinkles', 'frosting', 'gummy worms']
+  # end
+  #
+  # def vanilla
+  #   toppings
+  # end
