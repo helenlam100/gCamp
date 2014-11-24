191a6dddc6407382db2a604d422bb625abdf8660
Helen Lam <helen.f.lam100@gmail.com>
2014-11-23 18:36:48 -0800

    replace the array of strings with an array of Quote objects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,10 @@
 class WelcomeController < ApplicationController
-  def home
-    @quotes = [["Failure is not an option. Everyone has to succeed.", "-Arnold Schwarzeneggar"],
-    ["Your time is limited, so don't waste it living someone else's life.", "-Steve Jobs"],
-    ["Better Ingredients, Better Pizza", "-Pap John's"],
-    ]
-  end
-end
 
-  # def toppings
-  #   @toppings = ['sprinkles', 'frosting', 'gummy worms']
-  # end
-  #
-  # def vanilla
-  #   toppings
-  # end
+  def home
+    first_quote = Quote.new("Failure is not an option. Everyone has to succeed.", "-Arnold Schwarzeneggar")
+    second_quote = Quote.new("Your time is limited, so don't waste it living someone else's life.", "-Steve Jobs")
+    third_quote = Quote.new("Better Ingredients, Better Pizza", "-Pap John's")
+    @quotes = [first_quote, second_quote, third_quote]
+  end
+
+end
---
+++ app/models/quote.rb
@@ -0,0 +1,15 @@
+class Quote
+
+  attr_accessor :quote, :author
+  
+  def initialize(quote, author)
+    @quote = quote
+    @author = author
+  end
+
+
+
+
+
+
+end
