c824b0e4af9ca0dcbc4a79108c2dcc0d88d46802
Helen Lam <helen.f.lam100@gmail.com>
2014-12-03 13:30:59 -0800

    changed footer within html file as per changes requested

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
   def home
     first_quote = Quote.new("Failure is not an option. Everyone has to succeed.", "-Arnold Schwarzeneggar")
     second_quote = Quote.new("Your time is limited, so don't waste it living someone else's life.", "-Steve Jobs")
-    third_quote = Quote.new("Better Ingredients, Better Pizza", "-Pap John's")
+    third_quote = Quote.new("Better Ingredients, Better Pizza", "-Papa John")
     @quotes = [first_quote, second_quote, third_quote]
   end
-  
+
 end
