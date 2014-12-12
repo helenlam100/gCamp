4ff3d35e0fd0fdb3be87e244c51ac4633182524d
Helen Lam <helen.f.lam100@gmail.com>
2014-12-12 10:39:33 -0800

    a few changes and add to Travis CI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @users = User.all
@@ -14,37 +16,45 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to users_path, notice: 'User was successfully created.' }
-        format.json { render :index, status: :created, notice: 'User was successfully created.' }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 
-def show
 
-end
 
-def destroy
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path, notice: "User updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
     @user = User.find(params[:id])
     @user.delete
     flash[:notice] = "User was successfully deleted."
     redirect_to users_path
   end
 
-private
+  private
 
-def user_params
-  params.require(:user).permit(
-  :name,
-  :email,
-  :last_name
-  )
-end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+
+  def user_params
+    params.require(:user).permit(
+    :name,
+    :email,
+    :last_name
+    )
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true 
 end
+
+
+# 1,feature,"mvp,validations","Users must enter the first name, last name and email of a user  (on both new and edit)",,"![](https://galvanize.mybalsamiq.com/mockups/2436962.png?key=dd6f91232218fa4d6cbf663738e10e0cfca3e151)"
+# 1,feature,"mvp,validations",Users must enter a unique email for users,,""
+
+# create_table "users", force: true do |t|
+#   t.string   "name"
+#   t.string   "email"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+#   t.string   "last_name"
+# end
